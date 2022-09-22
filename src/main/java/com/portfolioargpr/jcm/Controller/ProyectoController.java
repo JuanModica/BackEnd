36803b79fde3eb848136319394999b339a532594
JuanModica <jcmodica18@gmail.com>
2022-09-22 02:17:50 -0300

    upload backend

--- src/main/java/com/portfolioargpr/jcm/Controller/ProyectoController.java
+++ src/main/java/com/portfolioargpr/jcm/Controller/ProyectoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendjcm.web.app")
 @RequestMapping("/proyecto")
 public class ProyectoController {
     @Autowired
